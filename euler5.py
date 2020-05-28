73fe88506fa568c22c82f22c786f91e0ebec8200
mp-ng <61499762+mp-ng@users.noreply.github.com>
2020-05-28 19:03:36 +0800

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May 24 14:37:17 2020
-
-@author: ngmingpok
-"""
 #Find the smallest positive integer that is evenly divisible by 1-20.
 def f():
     integer = 2520
